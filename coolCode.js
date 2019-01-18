fbf091609af6ac7e893811a7dbb0386446498f45
shinghao <sohshinghao@users.github.com>
2019-01-18 23:48:56 +0900

    Update coolCode.js
    Best code i have ever written

--- coolCode.js
+++ coolCode.js
@@ -1,2 +1,10 @@
+var jedi, kumamon;
+
 console.log("hi");
 console.log("kumamon is the best");
+if (jedi === kumamon)
+{
+    console.log("KUMAMOOOONNNNNNNN");
+} else {
+    console.log("Jedi is luke skywalker")
+}
